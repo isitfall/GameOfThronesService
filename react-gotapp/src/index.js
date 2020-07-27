578278736c80a8d6cfa017188051b4553ba9313a
isitfall <PDAnd4@yandex.ru>
2020-07-27 22:55:09 +0300

    initial commit

---
+++ react-gotapp/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
+
+//styles
+
+import './components/charDetails/charDetails.css'
+import './components/itemList/itemList.css'
+import './components/randomChar/randomChar.css'
+
+ReactDOM.render(<App />, document.getElementById('root'));
